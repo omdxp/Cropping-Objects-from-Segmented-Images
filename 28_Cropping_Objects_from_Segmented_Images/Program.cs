04c5750201d3c96346781af29492cdc188bd5d05
Omar-Belghaouti <bel_omar18@yahoo.com>
2019-03-20 15:03:17 +0100

    Add project files.

---
+++ 28_Cropping_Objects_from_Segmented_Images/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _28_Cropping_Objects_from_Segmented_Images
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
